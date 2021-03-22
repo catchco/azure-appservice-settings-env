48edd7188a38c751edb939803e0f8f3ca4b6277c
Josh Rogers <joshlrogers@outlook.com>
2021-03-22 12:14:50 -0500

    Adding additional logging/debug messages.

--- src/__tests__/appsettings-builder.test.ts
+++ src/__tests__/appsettings-builder.test.ts
@@ -1,8 +1,10 @@
+import * as core from '@actions/core';
 import fs from 'fs';
 import { AppConfiguration, AppSetting } from '../main';
 import { AppSettingsBuilder } from '../utilities/appsettings-builder';
 
 jest.mock('fs');
+jest.mock('@actions/core');
 
 describe('app settings builder', () => {
     describe('removed app settings', () => {
@@ -45,7 +47,7 @@ describe('app settings builder', () => {
                     slotSetting: false,
                     value: 'value'
                 }
-            ];            
+            ];
         });
 
         test('should return a value', () => {
@@ -63,91 +65,170 @@ describe('app settings builder', () => {
     });
 
     describe('new app settings', () => {
-        const builder: AppSettingsBuilder = new AppSettingsBuilder();
-        let existingAppSettings: AppSetting[];
-        let environmentAppSettings: AppSetting[];
 
-        beforeEach(() => {
-            existingAppSettings = [
-                {
-                    name: 'UPPERCASE_SETTING',
-                    slotSetting: false,
-                    value: 'value'
-                },
-                {
-                    name: 'lowercase_setting',
-                    slotSetting: false,
-                    value: 'value'
-                },
-                {
-                    name: 'variablecase_SETTING',
-                    slotSetting: false,
-                    value: 'value'
-                },
-                {
-                    name: 'unmatched_environment_VARIABLE',
-                    slotSetting: false,
-                    value: 'value'
-                },
-                {
-                    name: 'slot_setting',
-                    slotSetting: true,
-                    value: 'value'
-                }
-            ];
+        describe('bad property casing', () => { 
+            const builder: AppSettingsBuilder = new AppSettingsBuilder();
+            let existingAppSettings: AppSetting[];
+            let environmentAppSettings: AppSetting[];
 
-            environmentAppSettings = [
-                {
-                    name: 'uppercase_setting',
-                    slotSetting: false,
-                    value: 'value-new'
-                },
-                {
-                    name: 'LOWERCASE_SETTING',
-                    slotSetting: false,
-                    value: 'value-new'
-                },
-                {
-                    name: 'VARIABLECASE_setting',
-                    slotSetting: false,
-                    value: 'value-new'
-                },
-                {
-                    name: 'slot_setting',
-                    slotSetting: true,
-                    value: 'value-new'
-                }
-            ];
+            beforeEach(() => {
+                existingAppSettings = [
+                    {
+                        name: 'UPPERCASE_SETTING',
+                        slotSetting: false,
+                        value: 'value'
+                    },
+                    {
+                        name: 'lowercase_setting',
+                        slotSetting: false,
+                        value: 'value'
+                    },
+                    {
+                        name: 'variablecase_SETTING',
+                        slotSetting: false,
+                        value: 'value'
+                    },
+                    {
+                        name: 'unmatched_environment_VARIABLE',
+                        slotSetting: false,
+                        value: 'value'
+                    },
+                    {
+                        name: 'slot_setting',
+                        slotSetting: true,
+                        value: 'value'
+                    }
+                ];
 
-            const fileAppSettings: AppConfiguration = {
-                appSettings: existingAppSettings
-            };
+                environmentAppSettings = [
+                    {
+                        name: 'uppercase_setting',
+                        slotSetting: false,
+                        value: 'value-new'
+                    },
+                    {
+                        name: 'LOWERCASE_SETTING',
+                        slotSetting: false,
+                        value: 'value-new'
+                    },
+                    {
+                        name: 'VARIABLECASE_setting',
+                        slotSetting: false,
+                        value: 'value-new'
+                    },
+                    {
+                        name: 'slot_setting',
+                        slotSetting: true,
+                        value: 'value-new'
+                    }
+                ];
 
-            (fs.readFileSync as jest.Mock).mockReturnValue(JSON.stringify(fileAppSettings));
+                const fileAppSettings = {
+                    appsettings: existingAppSettings
+                };
+
+                (fs.readFileSync as jest.Mock).mockReturnValue(JSON.stringify(fileAppSettings));
+            });
+
+            test('should warn on unmatched propery', () => {
+                let logWarning = '';
+                (core.warning as jest.Mock).mockImplementation(cb => {
+                    logWarning = cb;
+                });
+                
+                const result = builder.buildNewAppSettings('file.json', environmentAppSettings);                
+
+                expect(logWarning).toBe('An appSettings property was not found on the configuration object. If this was intentional you may ignore this warning, otherwise please check the casing of your property names.')
+            })
         });
 
-        test('should return a value', () => {
-            const result = builder.buildNewAppSettings('file.json', environmentAppSettings);
-            expect(result).not.toBeNull();
-        })
+        describe('proper property casing', () => {
+            const builder: AppSettingsBuilder = new AppSettingsBuilder();
+            let existingAppSettings: AppSetting[];
+            let environmentAppSettings: AppSetting[];
 
-        test('should add file setting regardless of match in environment', () => {
-            const result = builder.buildNewAppSettings('file.json', environmentAppSettings);
-            
-            const unmatchedIndex = result.appSettings.findIndex(setting => setting.name === 'unmatched_environment_VARIABLE');
+            beforeEach(() => {
+                existingAppSettings = [
+                    {
+                        name: 'UPPERCASE_SETTING',
+                        slotSetting: false,
+                        value: 'value'
+                    },
+                    {
+                        name: 'lowercase_setting',
+                        slotSetting: false,
+                        value: 'value'
+                    },
+                    {
+                        name: 'variablecase_SETTING',
+                        slotSetting: false,
+                        value: 'value'
+                    },
+                    {
+                        name: 'unmatched_environment_VARIABLE',
+                        slotSetting: false,
+                        value: 'value'
+                    },
+                    {
+                        name: 'slot_setting',
+                        slotSetting: true,
+                        value: 'value'
+                    }
+                ];
 
-            expect(unmatchedIndex).not.toBe(-1);
-            if(unmatchedIndex != -1) {
-                expect(result.appSettings[unmatchedIndex].value).toBe('value');
-            }
-        });
+                environmentAppSettings = [
+                    {
+                        name: 'uppercase_setting',
+                        slotSetting: false,
+                        value: 'value-new'
+                    },
+                    {
+                        name: 'LOWERCASE_SETTING',
+                        slotSetting: false,
+                        value: 'value-new'
+                    },
+                    {
+                        name: 'VARIABLECASE_setting',
+                        slotSetting: false,
+                        value: 'value-new'
+                    },
+                    {
+                        name: 'slot_setting',
+                        slotSetting: true,
+                        value: 'value-new'
+                    }
+                ];
 
-        test('should patch value of environment matched app setting', () => {
-            const result = builder.buildNewAppSettings('file.json', environmentAppSettings);
+                const fileAppSettings: AppConfiguration = {
+                    appSettings: existingAppSettings
+                };
 
-            const matchedIndex = result.appSettings.findIndex(setting => setting.name === 'slot_setting');
+                (fs.readFileSync as jest.Mock).mockReturnValue(JSON.stringify(fileAppSettings));
+            });
 
-            expect(result.appSettings[matchedIndex].value).toBe('value-new');
+            test('should return a value', () => {
+                const result = builder.buildNewAppSettings('file.json', environmentAppSettings);
+                expect(result).not.toBeNull();
+            })
+
+            test('should add file setting regardless of match in environment', () => {
+                const result = builder.buildNewAppSettings('file.json', environmentAppSettings);
+
+                const unmatchedIndex = result.appSettings.findIndex(setting => setting.name === 'unmatched_environment_VARIABLE');
+
+                expect(unmatchedIndex).not.toBe(-1);
+                if (unmatchedIndex != -1) {
+                    expect(result.appSettings[unmatchedIndex].value).toBe('value');
+                }
+            });
+
+            test('should patch value of environment matched app setting', () => {
+                const result = builder.buildNewAppSettings('file.json', environmentAppSettings);
+
+                const matchedIndex = result.appSettings.findIndex(setting => setting.name === 'slot_setting');
+
+                expect(result.appSettings[matchedIndex].value).toBe('value-new');
+            });
         });
     });
 });
